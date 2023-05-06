231162d3480ac637db684bb1a1acd3b7e0a1450a
levinhkhangzz <levinhkhang631@gmail.com>
2023-05-06 08:50:47 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ README.md
@@ -0,0 +1,4 @@
+# NUMEROLOGY
+https://hovinhthanh7893.github.io/numerology/
+
+Updated February 2023
---
+++ app.js
@@ -0,0 +1,14 @@
+const app = Vue.createApp({
+    data() {
+        return {
+
+        }
+    },
+    method: {
+        rulingNumber() {
+            
+        }
+    }
+})
+
+app.mount('#app')
